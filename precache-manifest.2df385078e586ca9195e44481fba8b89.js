f4eca160d91eea5704abbaff7e93fdf5dc854a5e
tree bagas <tree.bagas@gmail.com>
2020-02-13 15:37:33 +0700

    Updates

---
+++ precache-manifest.2df385078e586ca9195e44481fba8b89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "142159f158903f436868cd7a218c9535",
+    "url": "/sap/index.html"
+  },
+  {
+    "revision": "0f1d80e44ee8899f616f",
+    "url": "/sap/static/js/2.f31b3dd6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/sap/static/js/2.f31b3dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc26a64fad013b47f098",
+    "url": "/sap/static/js/main.e128c0c5.chunk.js"
+  },
+  {
+    "revision": "76f9496f1d71462959c6",
+    "url": "/sap/static/js/runtime-main.64124d8e.js"
+  }
+]);
